e8e66b1a3a726ebd2b393e069720d3dce43d9efa
Wind_AF <84134093+WindAFeng@users.noreply.github.com>
2026-04-14 11:27:25 +0800

    更新代码

---
+++ webui/src/components/Upload.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {NUpload, NUploadDragger, NText} from "naive-ui";
+</script>
+
+<template>
+  <n-upload
+      multiple
+      directory-dnd
+      action=""
+      :max="5"
+  >
+    <n-upload-dragger>
+      <div style="margin-bottom: 12px">
+      </div>
+      <n-text style="font-size: 16px">
+        点击或者拖动文件到该区域来上传
+      </n-text>
+    </n-upload-dragger>
+  </n-upload>
+</template>
+
+<style scoped>
+</style>
